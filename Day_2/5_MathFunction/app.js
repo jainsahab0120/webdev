6c3d005babaa21bc1db84c4e3e37acb907e04282
Aryan Jain <aryan.ajtdl156@gmail.com>
2024-06-06 11:23:54 +0530

    day 2

--- Day_2/5_MathFunction/app.js
+++ Day_2/5_MathFunction/app.js
@@ -10,4 +10,30 @@ console.log(Math.round(pi)); //3
 console.log(Math.min(12,20,10 ,20)); // 10
 console.log(Math.sqrt(20)); //4.47213595499958
 
+console.log(Math.ceil(20.22));//21
+console.log(Math.floor(20.02)); // 20
+
+//Math.random
+
+let a  = Math.random();
+console.log(a); // 0 to 1 (excluded 1)
+
+//22 to 27
+
+let ans = Math.random() *5 +22;
+console.log(Math.floor(ans));
+
+prompt("enter your age");
+// this accept a string
+
+// but we can use parseInt and parseFloat to convert it 
+
+let age = parseInt(prompt("enter your age"));
+console.log(typeof(age));
+
+let age1 = parseFloat(prompt("enter your age"));
+console.log(typeof(age1));
+
+// && and || mein whi c wala logic chalta mtlb result mil gya to code age jayga nhi if ya else if mein 
+
 
